ef2eb35cdae0efeb95b13c50ef4f225747fc174b
icld <ian@icldesign.com>
2021-05-12 11:30:22 -0700

    change search to class

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './SearchForm.css'
 
-const SearchForm = () => (
-    <div>
-        <h1>Search Form Component</h1>
-    </div>
+class SearchForm extends Component {
 
-)
+    render() {
+        return (
+            <div>
+                <h1>Search Form Component</h1>
+            </div>
+        )
+    }
+
+}
 
 export default SearchForm;
