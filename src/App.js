53371bb56e1edda490573604cb54eef3324c732a
icld <ian@icldesign.com>
2021-05-15 15:04:47 -0700

    mess up my routes

--- src/App.js
+++ src/App.js
@@ -17,106 +17,36 @@ import NotFound from './Components/NotFound/NotFound.js';
 
 
 
-export default class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      photos: [],
-      cats: [],
-      dogs: [],
-      buzzards: [],
-      computers: []
-      // loading: true
-    }
-  }
-
-  componentDidMount() {
-    this.performSearch();
-    this.getCats();
-    this.getBuzzards();
-    this.getDogs()
-    this.getComputers()
-  }
-
-  getCats = () => {
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=cats&per_page=24&format=json&nojsoncallback=1
-  `)
-      .then(response => {
-        this.setState({
-          cats: response.data.photos.photo
-        })
-      })
-  }
-
-  getComputers = () => {
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=computers&per_page=24&format=json&nojsoncallback=1
-  `)
-      .then(response => {
-        this.setState({
-          computers: response.data.photos.photo
-        })
-      })
-  }
-  getBuzzards = () => {
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=buzzards&per_page=24&format=json&nojsoncallback=1
-  `)
-      .then(response => {
-        this.setState({
-          buzzards: response.data.photos.photo
-        })
-      })
-  }
-
-  getDogs = () => {
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=dogs&per_page=24&format=json&nojsoncallback=1
-  `)
-      .then(response => {
-        this.setState({
-          dogs: response.data.photos.photo
-        })
-      })
-  }
-
-  performSearch = (query = 'funny cats') => {
-    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1
-  `)
-      .then(response => {
-        this.setState({
-          photos: response.data.photos.photo
-        })
-      })
-      .catch(error => {
-        console.log('Error fetching and parsing data', error);
-      });
-  }
 
 
-  render() {
-    return (
-      <BrowserRouter>
+const App = () => {
 
-        <div className="container" >
-          <SearchForm onSearch={this.performSearch} />
-          <MainNav />
-          <Switch>
 
-            <Route exact path='/' render={() => <PhotoContainer title='Search something' data={this.state.photos} />} />
-            <Route path='/search/:query' render={() => <PhotoContainer data={this.state.photos} />} />
-            <Route exact path='/cats' render={() => <PhotoContainer title='cats' data={this.state.cats} />} />
-            <Route exact path='/dogs' render={() => <PhotoContainer title='dogs' data={this.state.dogs} />} />
-            <Route exact path='/buzzards' render={() => <PhotoContainer title='buzzards' data={this.state.buzzards} />} />
-            <Route exact path='/computers' render={() => <PhotoContainer title='computers' data={this.state.computers} />} />
-            <Route component={NotFound} />
 
-          </Switch>
-          {/* <PhotoContainer data={this.state.photos} /> */}
-        </div>
+  return (
+    <BrowserRouter>
 
-      </BrowserRouter>
+      <div className="container" >
+        <SearchForm onSearch={this.performSearch} />
+        <MainNav />
+        <Switch>
 
-    );
-  }
+          <Route exact path='/' render={() => <PhotoContainer title='Search something' data={this.state.photos} />} />
+          <Route path='/search/:query' render={() => <PhotoContainer />} />
+          <Route exact path='/cats' render={() => <PhotoContainer />} />
+          <Route exact path='/dogs' render={() => <PhotoContainer />} />
+          <Route exact path='/buzzards' render={() => <PhotoContainer />} />
+          <Route exact path='/computers' render={() => <PhotoContainer />} />
+          <Route component={NotFound} />
+
+        </Switch>
+        {/* <PhotoContainer data={this.state.photos} /> */}
+      </div>
+
+    </BrowserRouter>
+
+  );
+
 }
 
-
+export default App;
