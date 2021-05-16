e15f5e2f9b210280a2c987014c0cd2f7c306d929
icld <ian@icldesign.com>
2021-05-15 17:32:01 -0700

    ?

--- src/Components/MainNav/MainNav.js
+++ src/Components/MainNav/MainNav.js
@@ -14,7 +14,7 @@ const MainNav = (props) => (
     <div>
         <nav className="main-nav">
             <ul>
-                <li><NavLink to='/cats' onClick={() => props.onSearch('cats')}>Cats</NavLink></li>
+                <li><NavLink to='/cats' onClick={() => console.log('cats')}>Cats</NavLink></li>
                 <li><NavLink to='/dogs'>Dogs</NavLink></li>
                 <li><NavLink to='/computers'>Computers</NavLink></li>
                 <li><NavLink to='/buzzards'>Buzzards</NavLink></li>
--- src/Components/PhotoContainer/PhotoContainer.js
+++ src/Components/PhotoContainer/PhotoContainer.js
@@ -3,37 +3,37 @@ import React from 'react';
 import PhotoCard from '../PhotoCard/PhotoCard';
 import NotFound from '../NotFound/NotFound'
 import './PhotoContainer.css'
-import { Consumer } from '../Context';
+
 
 const PhotoContainer = () => {
 
     return (
-        <Consumer>
-            {({ photos }) => {
-                const results = photos;
-                let photoss;
-                const title = photos.title
+
+        {(props) => {
+    const results = photos;
+    let photos;
+    const title = photos.title
 
 
-                if (results.length > 0) {
-                    photoss = results.map(photo =>
-                        <PhotoCard server={photo.server} secret={photo.secret} id={photo.id} key={photo.id} />
-                    )
-                } else photos = <NotFound />
+    if (results.length > 0) {
+        photos = results.map(photo =>
+            <PhotoCard server={photo.server} secret={photo.secret} id={photo.id} key={photo.id} />
+        )
+    } else photos = <NotFound />
 
-                return (
-                    <div className="photo-container">
-                        <h2>{title}</h2>
-                        <ul>
-                            {photoss}
+    return (
+        <div className="photo-container">
+            <h2>{title}</h2>
+            <ul>
+                {photos\s}
                         </ul>
-                    </div>
-                )
-            }
+        </div>
+    )
+}
 
             }
 
-        </Consumer>
+        </Consumer >
     )
 
 
